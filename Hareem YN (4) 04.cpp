ef92d3450d71601cdfcfd7dafb1acda21157442a
HareemYounas <159714791+HareemYounas@users.noreply.github.com>
2024-08-28 10:02:54 -0700

    Add files via upload

---
+++ Hareem YN (4) 04.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int value(int);
+int main()
+{
+	int x;
+	cout<<"Enter an integer: ";
+	cin>>x;
+	value(x);
+}
+int value(int x)
+{
+	int p,n;
+	p=x-1;
+	n=x+1;
+	cout<<"The number before "<<x<<"is" <<p<<endl;
+	cout<<"The number after "<<x<<"is" <<n<<endl;
+}
